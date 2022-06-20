b2c1f8bdb5b0b716176a5b21dfc5dda67d27281a
giovanni.chierico <giovanni.chierico@gmail.com>
2022-06-20 22:52:08 +0200

    SmartArray

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import {SmartArray} from "./StoryMapper";
 
-describe("some name", () => {
-  it('the test', () => {expect("abc").toEqual("abc")})
+describe("Smart Array", () => {
+    const a = "a";
+
+    describe("push", () => {
+        const sa = new SmartArray<String>();
+        sa.push(a);
+        sa.push("b");
+        it('toString', () => {
+            expect(sa.toString()).toEqual("[a,b]")
+        })
+    })
+
+    describe("remove", () => {
+        const sa = new SmartArray<String>();
+        sa.push(a);
+        sa.push("b");
+        sa.remove(a);
+        it('toString', () => {
+            expect(sa.toString()).toEqual("[b]")
+        })
+    });
+
+    describe("add", () => {
+        const sa = new SmartArray<String>();
+        sa.push(a);
+        sa.push("b");
+
+        it('add 0', () => {
+            sa.add("c", 0);
+            expect(sa.toString()).toEqual("[c,a,b]")
+        });
+        it('add end', () => {
+            sa.add("e", 10);
+            expect(sa.toString()).toEqual("[c,a,b,e]")
+        });
+        it('add middle', () => {
+            sa.add("x", 1);
+            expect(sa.toString()).toEqual("[c,x,a,b,e]")
+        });
+    });
+
 });
--- src/StoryMapper.ts
+++ src/StoryMapper.ts
@@ -1,31 +1,111 @@
-export class Version {
-    name: String;
-    steps:Map<number,Step> = new Map();
+export class SmartArray<T> {
+    items: T[] = [];
 
-    constructor(name:String){
+    push(item: T) {
+        this.items.push(item);
+    }
+
+    add(item: T, position: number) {
+        this.items.splice(position, 0, item);
+    }
+
+    remove(item: T) {
+        this.items = this.items.filter((a: T) => a !== item);
+    }
+
+    toString(): String {
+        return "[" + this.items.toString() + "]";
+    }
+}
+
+// allow to maintain items in a list and reorder them
+interface ISortable {
+    getPosition(): number;
+
+    getName(): String;
+}
+
+class Sortable implements ISortable {
+    position: number;
+    name: String;
+
+    constructor(position: number, name: String) {
+        this.position = position;
         this.name = name;
     }
 
-    addNote(note:Note){
+    getPosition(): number {
+        return this.position;
+    }
 
+    getName(): String {
+        return this.name;
     }
 }
 
-// the
-export class Step {
-    sequenceNumber: number;
+class SortableSet<T extends Sortable> {
+    items: Set<T> = new Set<T>();
+    list: T[] = [];
+    // track if list needs to be rebuilt
+    dirty: boolean = false;
 
-    constructor(id:number){
-        this.sequenceNumber = id;
+    add(item: T) {
+        this.dirty = true;
+        this.items.add(item);
     }
+
+    remove(item: T): void {
+        this.dirty = true;
+        this.items.delete(item);
+    }
+
+    getList(): T[] {
+        if (this.dirty) {
+            // recompute list only when needed
+            this.dirty = false;
+            this.list = Array.from(this.items).sort((a: T, b: T) => a.position - b.position);
+        }
+        return this.list;
+    }
+
 }
 
 // the note that describes the actual work
-export class Note {
+export class Note extends Sortable {
     step: Step;
+    version: Version;
 
-    constructor(step:Step){
+    constructor(position: number, name: String, step: Step, version: Version) {
+        super(position, name);
         this.step = step;
+        this.version = version;
     }
+}
+
+class SortableSetSortable extends SortableSet<Note> implements ISortable {
+    sortable: Sortable;
+
+    constructor(position: number, name: String) {
+        super();
+        this.sortable = new Sortable(position, name);
+    }
+
+    add(note: Note) {
+        this.add(note);
+    }
+
+    getName(): String {
+        return this.sortable.getName();
+    }
+
+    getPosition(): number {
+        return this.sortable.getPosition();
+    }
+}
+
+class Version extends SortableSetSortable {
+}
+
+class Step extends SortableSetSortable {
 }
 
